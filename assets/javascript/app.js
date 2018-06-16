ffc650ed3a925ce7a4249dbd8d10ad2fcaff060f
Ansel Adams <dj@volaciousbrands.com>
2018-06-16 15:02:01 -0500

    added start date and months worked calc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,14 @@ var config = {
 
   var database = firebase.database();
 
+  function calculateMonthsWorked(startDay) {
+    var monthsWorked = moment(moment(), 'months').diff(startDay, "months");
+    // Ternary statement to filter out the NAN values.  They are expressions, they evaluate to a value.
+    // Not an if-then statement, it is an evaluation.
+    return isNaN(monthsWorked) ? 0 : monthsWorked;
+}
 $("#add-user").on("click", function(event) {
-   // event.preventDefault();
+   event.preventDefault();
     console.log("hello")
     // Display the info to the HTML
     employeeName = $("#name-input").val().trim();
@@ -20,19 +26,20 @@ $("#add-user").on("click", function(event) {
     employeeStartDate = $("#start-date-input").val().trim();
     employeeMonthlyRate = $("#monthly-rate-input").val().trim();
 
+    // var startDay = moment(employeeStartDate).diff(moment(), "months");
+    // console.log(startDay);
+    // var monthsWorked = moment(startDay);
+    // console.log(monthsWorked);
+    // var monthsWorked = calculateMonthsWorked(startDay);
+
     // Pushing new data to the database
     database.ref().push({
         employeeName : employeeName,
         employeeRole : employeeRole,
-        employeeStartDate :employeeStartDate,
-        employeeMonthlyRate :employeeMonthlyRate,
-        dataAdded: firebase.database.ServerValue.TIMESTAMP
+        employeeStartDate : employeeStartDate,
+        employeeMonthlyRate : employeeMonthlyRate,
+        dataAdded : firebase.database.ServerValue.TIMESTAMP
     });
-
-
-
-    
-
     // Clear the input values
     // $(".form-control").empty();
 });
@@ -43,9 +50,12 @@ $("#add-user").on("click", function(event) {
 // $("#nameDisplay").append(newP);
 
 database.ref().on("child_added", function(snapshot) {
-    console.log(snapshot.val())
+    var monthsWorked = calculateMonthsWorked(snapshot.val().employeeStartDate);
+    console.log("about to append to the DOM")
+    // console.log(snapshot.val())
     $("#nameDisplay").append("<p>" + snapshot.val().employeeName + "</p>");
     $("#roleDisplay").append("<p>" + snapshot.val().employeeRole + "</p>");
     $("#startDateDisplay").append("<p>" + snapshot.val().employeeStartDate + "</p>");
     $("#monthlyRateDisplay").append("<p>" + snapshot.val().employeeMonthlyRate + "</p>")
+    $("#monthsWorkedDisplay").append("<p>" + monthsWorked + " months" + "</p>");
     });
