789f920110e19b610a16d3f67fabd9f5abf6ba87
Ansel Adams <dj@volaciousbrands.com>
2018-06-16 16:06:39 -0500

    added total billed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,12 @@ var config = {
     // Not an if-then statement, it is an evaluation.
     return isNaN(monthsWorked) ? 0 : monthsWorked;
 }
+
+function calculateTotalBilled(monthsWorked, employeeMonthlyRate) {
+    var totalBilled = (monthsWorked * employeeMonthlyRate);
+    return totalBilled;
+}
+
 $("#add-user").on("click", function(event) {
    event.preventDefault();
     console.log("hello")
@@ -52,10 +58,14 @@ $("#add-user").on("click", function(event) {
 database.ref().on("child_added", function(snapshot) {
     var monthsWorked = calculateMonthsWorked(snapshot.val().employeeStartDate);
     console.log("about to append to the DOM")
+
+    var totalBilled = calculateTotalBilled(monthsWorked, snapshot.val().employeeMonthlyRate)
+    console.log(totalBilled);
     // console.log(snapshot.val())
     $("#nameDisplay").append("<p>" + snapshot.val().employeeName + "</p>");
     $("#roleDisplay").append("<p>" + snapshot.val().employeeRole + "</p>");
     $("#startDateDisplay").append("<p>" + snapshot.val().employeeStartDate + "</p>");
-    $("#monthlyRateDisplay").append("<p>" + snapshot.val().employeeMonthlyRate + "</p>")
+    $("#monthlyRateDisplay").append("<p>" + snapshot.val().employeeMonthlyRate + "</p>");
     $("#monthsWorkedDisplay").append("<p>" + monthsWorked + " months" + "</p>");
+    $("#totalBilledDisplay").append("<p>" + "$" + totalBilled); 
     });
